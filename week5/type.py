18bf96a690217bb358b15e29b848dd0dfef8152a
l3nomumi <bruna311065@gmail.com>
2026-02-16 15:39:15 +0700

    Add files via upload

---
+++ week5/type.py
@@ -0,0 +1,21 @@
+FullName = "Watcharin Sukpasoed"
+Stdudent = 684245012
+Gpax = 3.38
+Age = 18
+
+if type(FullName) == str:
+  print("Watcharin Sukpasoed is Str")
+else:
+  print("Watcharin Sukpasoed is not Str")
+if type(Stdudent) == complex:
+  print("684245012 is complex")
+else:
+  print("684245012 is not complex")
+if type(Gpax) == float:
+  print("3.25 is float")
+else:
+  print("3.25 is not float")
+if type(Age) == int:
+  print("18 is int")
+else:
+  print("18 is not int")
